79c48e516c52816b5580c4f916214d21c84f398d
Gregory Machin <gregory.d.machin@gmail.com>
2017-12-13 21:14:54 +1300

    Scratch, drafting some filters, learing Get-ChildItem Filtering Exclude

--- Supervisor.ps1
+++ Supervisor.ps1
@@ -1,4 +1,7 @@
-$FilesList = Get-ChildItem -Path c:\temp -Recurse -Filter "*.LOCK"  
+$ExcludeFilter = "*.LOCK","*.config","*.bat","*.dat"
+
+
+$FilesList = Get-ChildItem -Path c:\temp -Recurse -Exclude $ExcludeFilter  
 $FilesList | Get-Member
 
 foreach ($File in $FilesList){
