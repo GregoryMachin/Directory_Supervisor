feb2784b76189c108baedb107868412c5779c08c
Gregory Machin <gregory.d.machin@gmail.com>
2017-12-13 22:00:31 +1300

    Scratch testing ways to filter no dates

--- Supervisor.ps1
+++ Supervisor.ps1
@@ -1,12 +1,16 @@
 $ExcludeFilter = "*.LOCK","*.config","*.bat","*.dat"
 $IncludeFilter = "*.exe"
-
+[DateTime]$LastWritetimeUpper = 'December 30, 2017'
+[DateTime]$LastWritetimelower = 'December 1, 2017'
+$FileSource = "C:\Users\clubb\AppData\Local\Temp"
 #$FilesList = Get-ChildItem -Path c:\temp -Recurse -Exclude $ExcludeFilter  
-$FilesList = Get-ChildItem -Path c:\temp -Recurse -Include $IncludeFilter  
+#$FilesList = Get-ChildItem -Path c:\temp -Recurse -Include $IncludeFilter 
+$FilesList = Get-ChildItem -Path $FileSource -Recurse    
 
-$FilesList | Get-Member
+#$FilesList | Get-Member
 
 foreach ($File in $FilesList){
 $File.Fullname
+$File | ForEach-Object { $_.lastwritetime -gt $LastWritetimelower -AND $_.lastwritetime -lt  $LastWritetimeUpper}
 
 }
